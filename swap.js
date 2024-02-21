c86d3774b6d789fc265f516ecfc9ee81a3805ee5
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-02-22 00:23:17 +0600

    swap added

---
+++ swap.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 7;
+console.log(a, b);
+const temp = a;
+a = b;
+b = temp;
+console.log(a, b);
+
+// ----------------
+
+let x = 15;
+let y = 17;
+console.log(x, y);
+[x, y] = [y, x];
+console.log(x, y);
